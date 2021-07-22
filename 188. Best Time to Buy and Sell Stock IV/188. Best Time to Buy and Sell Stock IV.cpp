c930cbf5161e0d4773abe79acb543ebe70f2e2ae
gengyouchou <52925394+gengyouchou@users.noreply.github.com>
2021-07-22 21:53:36 +0800

    Update 188. Best Time to Buy and Sell Stock IV.cpp

--- 188. Best Time to Buy and Sell Stock IV/188. Best Time to Buy and Sell Stock IV.cpp
+++ 188. Best Time to Buy and Sell Stock IV/188. Best Time to Buy and Sell Stock IV.cpp
@@ -1,1 +1,19 @@
-
+class Solution {
+public:
+    int maxProfit(int k, vector<int>& prices) {
+        
+        int n=prices.size();
+        vector<vector<int>> dp(k+1,vector<int>(n+1,0));
+        
+        for(int i=1;i<=k;++i){
+            int hold=INT_MIN;
+            for(int j=1;j<=n;++j){
+                dp[i][j]=max(dp[i][j-1],prices[j-1]+hold);
+                hold=max(hold,dp[i-1][j]-prices[j-1]);
+            }
+        }
+        
+        return dp[k][n];
+        
+    }
+};
