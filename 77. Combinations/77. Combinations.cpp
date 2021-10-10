129e052e3ffe43e753160d08469563e6fd3cc913
gengyouchou <52925394+gengyouchou@users.noreply.github.com>
2021-10-10 19:49:42 +0800

    Create 77. Combinations.cpp

---
+++ 77. Combinations/77. Combinations.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> combine(int n, int k) {
+        vector<int> temp;
+        dfs(n,k,1,temp);
+        return res;
+    }
+private:
+    vector<vector<int>> res;
+    
+    void dfs(int n,int k,int i,vector<int>& temp){
+        if(temp.size()==k){
+            res.push_back(temp);
+        }
+        
+        for(;i<=n;++i){
+            temp.push_back(i);
+            dfs(n,k,i+1,temp);
+            temp.pop_back();
+        }
+    }
+};
